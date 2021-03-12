814c00315e85603dddbe1725f76543845368c9bb
Josh Finkel <jlf581@nyu.edu>
2021-03-12 10:59:04 -0500

    Added 2 levels, ways to go between rooms, and one way ledges.

--- Assets/Scripts/TopDownPlayerBehavior.cs
+++ Assets/Scripts/TopDownPlayerBehavior.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TopDownPlayerBehavior : MonoBehaviour
 {
@@ -8,10 +9,44 @@ public class TopDownPlayerBehavior : MonoBehaviour
     //Rigidbody2D myRigidbody;
     public float speed;
 
+    public int numScenes;
+
+    public float endTimeReset;
+
+    float endTime;
+
+    bool countDown = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        endTime = endTimeReset;
         //myRigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R)){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if(Input.GetKeyDown(KeyCode.N)){
+            NextScene();
+        }
+        if(countDown == true){
+            endTime -= Time.deltaTime;
+            if(endTime <= 0){
+                NextScene();
+                endTime = endTimeReset;
+                countDown = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Finish"){
+            countDown = true;
+        }
     }
 
     void FixedUpdate()
@@ -29,5 +64,14 @@ public class TopDownPlayerBehavior : MonoBehaviour
         if(Input.GetKey(KeyCode.D)){
             transform.Translate(Vector3.right*Time.deltaTime*speed);
         }
+    }
+
+    void NextScene()
+    {
+        int nextIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            if(nextIndex > numScenes){
+                nextIndex = 0;
+            }
+        SceneManager.LoadScene(nextIndex);
     }
 }
---
+++ Assets/Scripts/WallBehavior.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBehavior : MonoBehaviour
+{
+
+    public float sightDist;
+
+    //public Vector3 targetMod;
+
+    public Transform target;
+
+    public BoxCollider2D col;
+
+    public float numOfRays;
+
+    public float raySeparation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //col = gameObject.GetComponent<BoxCollider2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        bool hitAnything = false;
+
+        //target = transform.position + targetMod;
+
+        //Vector3 rayOffset = new Vector3(0, (transform.localScale.y /2) + .1f, 0);
+
+            for (float i = 0; i < numOfRays; i+=raySeparation){
+                
+                //LEFT
+                Vector3 offPos= new Vector3(transform.position.x - i, transform.position.y, transform.position.z);
+                RaycastHit2D hit = Physics2D.Raycast(offPos, target.localPosition, sightDist);
+                Debug.DrawRay(offPos, target. localPosition, Color.blue);
+                if(hit.collider != null){
+                    if(hit.collider.gameObject.tag != "Wall"){
+                        col.enabled = true;
+                        hitAnything = true;
+                        Debug.Log(hit.collider.name);
+                    }
+                }
+                else {
+                    if(hitAnything == false){
+                        col.enabled = false;
+                    }
+                }
+
+                //RIGHT
+                Vector3 offPosR= new Vector3(transform.position.x + i, transform.position.y, transform.position.z);
+                RaycastHit2D hitR = Physics2D.Raycast(offPosR, target.localPosition, sightDist);
+                Debug.DrawRay(offPosR, target. localPosition, Color.blue);
+                if(hitR.collider != null){
+                    if(hitR.collider.gameObject.tag != "Wall"){
+                        col.enabled = true;
+                        hitAnything = true;
+                        Debug.Log(hitR.collider.name);
+                    }
+                }
+                else {
+                    if(hitAnything == false){
+                        col.enabled = false;
+                    }
+                }
+        }
+    }
+}
